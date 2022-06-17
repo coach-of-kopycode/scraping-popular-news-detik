0213a5ae9f51f29fdde9d7668ad1fa80928d2c19
todikun <todirahmat123@gmail.com>
2022-06-17 22:01:03 +0700

    Flask: Show data

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template
 
 
@@ -7,6 +9,37 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/detik')
+def scraping():
+    content = requests.get('https://www.detik.com/terpopuler')
+    soup = BeautifulSoup(content.text, 'html.parser')
+
+    last_update = soup.find('div', {'class': 'page__indeks-info font-base-semibold'}).text
+    print(f'Daftar berita terpopuler, {last_update}')
+
+    titles = soup.findAll(attrs={'class': 'media__title'})
+    times = soup.findAll(attrs={'class': 'media__date'})
+    print(f'Total berita {len(titles)}\n')
+
+    list_titles = []
+    list_times = []
+    i = 1
+    j = 1
+
+    for title in titles:
+        title = title.text.strip()
+        list_titles.append(title)
+        i = i + 1
+
+    for time in times:
+        time = time.text.strip()
+        list_times.append(time)
+        j = j + 1
+
+    new_list = [list_titles, list_times]
+    return render_template('index.html', last_update=last_update, new_list=new_list, list_titles=list_titles, list_times=list_times)
 
 
 if __name__ == '__main__':
